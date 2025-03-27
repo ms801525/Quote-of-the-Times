38bf6c2e7bf78f816248de78a97b3cb196dc8334
ms801525 <ms801525@dal.ca>
2025-03-27 17:48:23 -0300

    added random string generation

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+// Purpose: To display the current date and time on the webpage
 function updateTime() {
     const date = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -10,7 +11,20 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 
+
+
+//Temporary function to display a message on the webpage
 function displayMessage() {
     const message = "You miss 100% of the shots you don't take. - Wayne Gretzky";
     document.getElementById('quote').innerHTML = message;
+}
+
+function generateRandomString(length) {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+        document.getElementById('random').innerHTML = result;
+    }
+    return result;
 }
