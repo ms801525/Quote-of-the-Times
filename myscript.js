852157fda57d7471a5cd10b84a09b40c232154e6
ms801525 <ms801525@dal.ca>
2025-03-27 12:43:35 -0300

    CSS button changes

--- myscript.js
+++ myscript.js
@@ -9,3 +9,8 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
+
+function displayMessage() {
+    const message = "You miss 100% of the shots you don't take. - Wayne Gretzky";
+    document.getElementById('quote').innerHTML = message;
+}
