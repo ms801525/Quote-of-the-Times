f65973e8b8a7e0a24101cb6345a413c2fcf781ac
ms801525 <ms801525@dal.ca>
2025-03-31 16:46:25 -0300

    Pulling quote from JSON file with the click of a button

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+
 // Purpose: To display the current date and time on the webpage
 function updateTime() {
     const date = new Date();
@@ -13,11 +14,34 @@ updateTime();
 
 const button = document.getElementById('quote-button');
 
-let quotes = [
-    { author: "John Lennon", quote: "Life is what happens when you're busy making other plans." },
-    { author: "William Shakespeare", quote: "To be, or not to be, that is the question." },
-    { author: "Franklin D. Roosevelt", quote: "The only limit to our realization of tomorrow is our doubts of today." }
-];
+// let quotes = [
+//     { author: "John Lennon", quote: "Life is what happens when you're busy making other plans." },
+//     { author: "William Shakespeare", quote: "To be, or not to be, that is the question." },
+//     { author: "Franklin D. Roosevelt", quote: "The only limit to our realization of tomorrow is our doubts of today." }
+// ];
+
+// Fetch quotes from the JSON file
+//quotes JSON file was taken from https://gist.github.com/JakubPetriska/060958fd744ca34f099e947cd080b540
+let quotes = [];
+console.log('Fetching quotes from JSON file...');
+fetch('quotes.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+            console.log('Error fetching quotes:', response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        quotes = data;
+        console.log('Quotes loaded:', quotes);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        console.log('Error fetching quotes:', error);
+    });
+
+
 function getRandomQuote() {
     if(quotes.length === 0){
         alert("Quote is Loading");
@@ -25,9 +49,12 @@ function getRandomQuote() {
     }
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
+    console.log(randomIndex);
 
-    document.getElementById('quotedisplay').textContent = `"${randomQuote.quote}"`;
-    document.getElementById('author').textContent = `- ${randomQuote.author}`;
+    const elem = document.getElementById('quote');
+    elem.innerHTML = randomQuote.Quote + '<br><br> - ' + randomQuote.Author;
+    console.log(randomQuote);
+
 }
 
 function cooldown() {
