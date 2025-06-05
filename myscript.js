f7545469da9f96a3b6d14aa9d1acdf62065ec80f
ms801525 <ms801525@dal.ca>
2025-06-05 16:43:51 -0300

    new dev branch

--- myscript.js
+++ myscript.js
@@ -1,25 +1,5 @@
 console.log("✅ JS loaded!");
 
-    // document.addEventListener("DOMContentLoaded", function () {
-    //   console.log("✅ DOM ready!");
-
-    //   const acc = document.getElementsByClassName("accordion");
-    //   console.log("Found", acc.length, "accordion(s)");
-
-    //   for (let i = 0; i < acc.length; i++) {
-    //     acc[i].addEventListener("click", function () {
-    //       console.log("Accordion clicked");
-
-    //       this.classList.toggle("active");
-    //       const panel = this.nextElementSibling;
-    //       if (panel.style.display === "block") {
-    //         panel.style.display = "none";
-    //       } else {
-    //         panel.style.display = "block";
-    //       }
-    //     });
-    //   }
-    // });
 // Purpose: To display the current date and time on the webpage
 function updateTime() {
     const date = new Date();
